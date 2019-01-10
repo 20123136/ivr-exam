c22f9e1719bb9517e8853958e2794f5df87f3b8c
zhangna <zhangna@wilcom.com.cn>
2019-01-10 16:31:22 +0800

    img

---
+++ www/myapp.js
@@ -0,0 +1,14 @@
+const MYAPP = {
+    "nodeType": {
+        "start": "start",
+        "fangyin": "fangyin",
+        "caidan": "caidan",
+        "zhuanjie": "zhuanjie",
+        "shouhao": "shouhao",
+        "shibie": "shibie",
+        "panduan": "panduan",
+        "ziliucheng": "ziliucheng",
+        "guaduan": "guaduan"
+    },
+    "png": ".png",
+}
